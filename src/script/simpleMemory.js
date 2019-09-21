339a6ae98c7ee5a150d300fa12311fedb7461695
mingyue <1441656607@qq.com>
2019-09-22 02:22:41 +0800

    恢复统计数值

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -23,7 +23,7 @@ if (initCheck()) {
         '            </div>' +
         '            <!-- 统计 -->' +
         '            <div class="m" align="center">' +
-        '                 <a href="https://info.flagcounter.com/6W1G"><img src="https://s04.flagcounter.com/countxl/6W1G/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_12/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>' +
+        '                 <a href="https://info.flagcounter.com/6W1G"><img src="https://s04.flagcounter.com/countxl/NQjf/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>' +
         '            </div>' +
         '            <!-- 日历 -->' +
         '            <span id="calendar-box"></span>' +
