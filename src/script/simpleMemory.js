d3df43cab680be221401b4e72d0d4e293090f8b7
mingyue <1441656607@qq.com>
2019-09-22 02:29:00 +0800

    增加访客统计title

--- src/script/simpleMemory.js
+++ src/script/simpleMemory.js
@@ -27,7 +27,8 @@ if (initCheck()) {
         '            <div class="m-list-title"><span>找找看</span></div>' +
         '            <div class="m-icon-list" id="sb-sidebarSearchBox"></div>' +
 		'            <!-- 统计 -->' +
-        '            <div class="m" align="center">' +
+		'            <div class="m-list-title"><span>访客统计</span></div>' +
+        '            <div class="m-icon-list" align="center">' +
         '                 <a href="https://info.flagcounter.com/6W1G"><img src="https://s04.flagcounter.com/countxl/NQjf/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>' +
         '            </div>' +
         '            <!-- 最新随笔 -->' +
